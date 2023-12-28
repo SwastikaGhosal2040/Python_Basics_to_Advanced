558ce5fa6ccff10388175b2553af69d0e15224ae
Swastika Ghosal <ghosalswastika2040@gmail.com>
2023-12-28 08:37:27 +0530

    Add files via upload

---
+++ 3_stringConcat.py
@@ -0,0 +1,5 @@
+#print hello world using string concatination
+print("Hello"+" "+"world")#using double quote(using 3 strings)
+print('Hello'+' '+'world')#using single quote(using 3 strings)
+print('Hello '+'World')#put space inside Hello(using 2 strings)
+print('Hello'+' world')#put space inside world(using 2 strings)
