bbbdd9a41fca3fa08adee490cf47e5e7ca6c882e
Swastika Ghosal <ghosalswastika2040@gmail.com>
2023-12-29 15:01:19 +0530

    Add files via upload

---
+++ 5_swapStringconcat.py
@@ -0,0 +1,13 @@
+a = input('Enter value of a =')
+b = input('Enter value of b =')
+temp = a
+a = b 
+b = temp
+# using string concatenation
+# we an concatenate 'After swapping a = ' and a because both contains a string
+# a = input()....we know input function returns sring
+# so a contains string
+#part1 contains string
+# so we can concatenate these two
+print('After swapping a = ' + a)
+print('After swapping b = ' + b)
