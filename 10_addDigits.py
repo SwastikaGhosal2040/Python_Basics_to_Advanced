33a4270f251c7f5f9073356fed525b2c93e8ec4c
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-01 19:13:44 +0530

    Add files via upload

---
+++ 10_addDigits.py
@@ -0,0 +1,6 @@
+#Program to add digits of a 2 digit number 
+number = input('Take a 2 digit no. input =')
+digit1 = int(number[0])
+digit2 = int(number[1])
+sum = digit1 + digit2
+print('Sum of the digits = ',sum)
